d1069a8ea5ecb283bf5e1c271c5ffccbbff014ed
cyfflying0125 <49615626+cyfflying0125@users.noreply.github.com>
2022-03-06 02:15:40 +0800

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,6 +1,22 @@
 <?php
   include 'connect.php';
   date_default_timezone_set('Asia/Singapore');
+
+  if (!empty($_POST['title'])){
+    $title = $_POST['title'];
+    $date = $_POST['currentDate'];
+    $dateFormatted = date("Y-m-d", strtotime($date));
+    $startTime = $_POST['eventTime'];
+    $colour = $_POST['colour'];
+    $location = $_POST['location'];
+    $agenda = $_POST['agenda'];
+
+    $queryInsert = "INSERT INTO calendar(`title`, `date`, `start_time`, `colour`, `location`, `agenda`)
+      VALUES ('$title' ,'$dateFormatted','$startTime','$colour','$location','$agenda');";
+    //echo $queryInsert;
+    $result = $db->query($queryInsert);
+    echo '<script>alert("Event Added Successfully!")</script>';
+  }
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,6 +27,7 @@
     <link rel="stylesheet" href="css/mastercss.css">
     <link rel="stylesheet" href="css/calendarcss.css">
   	<title>Wedding Planner</title>
+
 	</head>
 	<body>
 
@@ -38,7 +55,7 @@
     <div id="leftcol">
     <div id="calendar">
       <div class="month">
-         <i class="prev"><img src="icon/back.png" width="32" height="32" style="" onclick="addEL();"> </i>
+         <i class="prev"><img src="icon/back.png" width="32" height="32"> </i>
          <div class="date">
            <h1>March</h1>
 
@@ -58,33 +75,58 @@
        <div class="days"></div>
     </div>
     <div id = list>
-      <h2>TIMELINE</h2>
+      <h2>UPCOMING</h2>
+      <hr style="border-top: 1px solid #F0EDED; opacity: 0.5; margin-bottom: 24px;">
+      <table>
+        <?php
+          $query = "SELECT * FROM calendar WHERE date >= CURDATE() ORDER BY date ASC, start_time";
+          $result = $db->query($query);
+          $num_results = $result->num_rows;
+          $datePrinted = null;
+          if($num_results == 0) {
+            echo " <tr><td>No event scheduled yet.</td><tr>";
+          } else {
+            for($i=0; $i<$num_results; $i++) {
+              $row = $result->fetch_assoc();
+        ?>
+              <tr><th>
+                <?php $date = $row['date'];
+                if($date != $datePrinted) {
+                  $dateFormatted = date("j M Y", strtotime("$date"));
+                  echo $dateFormatted;
+                }
+                $datePrinted = $date;  ?></th>
+
+                <td><span class="dot" style="background-color: <?php echo $row['colour'];?>"></span>
+                <?php
+                $time = $row['start_time'];
+                $timeFormatted = date("g:i A", strtotime("$time"));
+                echo $timeFormatted," <b> - ";
+                echo $row['title'],"</b><br>";
+                echo $row['location'],"<br>";
+                echo $row['agenda'];
+                ?></td>
+              </tr>
+        <?php
+            }
+          }
+        ?>
+      </table>
     </div>
-
-
   </div>
+
     <div id="schedule">
       <h2>Event Scheduled</h2>
-      <table class = "scheduled">
-        <tr><td><span class="dot"></span><b>Meeting with Photographer</b>
-                <hr>12:00pm
-                <br>Starbucks (Star Vista)
-                <br>Discuss pre-wedding photoshoot
-                </td></tr>
-        <tr><td><span class="dot"></span><b>Meeting with Photographer</b>
-                <hr>12:00pm
-                <br>Starbucks (Star Vista)
-                <br>Discuss pre-wedding photoshoot
-                </td></tr>
-      </table>
+      <table class = "scheduled" id = "event"></table>
       <h2>Add a New Event</h2>
-      <form action="addNew.php" method = "post">
+      <form action="calendar.php" method = "post">
       <table class="addNew">
-        <tr><th>Title* </th><td><input type="text" required></td></tr>
-        <tr><th>Colour </th><td><input id = "colorpicker" type="color" value ="#bbbbbb"></tr>
-        <tr><th>Date</th><td id="current"><?php echo date("d M Y");?></td></tr>
-        <tr><th>Start Time</th><td><input type="time" value="13:00"></td></tr>
-        <tr><th>Agenda </th><td><input type="textarea"></td></tr>
+        <tr><th>Title* </th><td><input type="text" name="title" required></td></tr>
+        <tr><th>Colour </th><td><input id = "colorpicker" name="colour" type="color" value ="#bbbbbb"></tr>
+        <tr><th>Date</th><td><input id="currentDate" name="currentDate" value='<?php echo date("d M Y");?>' onfocus="this.blur();"></td></tr>
+        <tr><th>Start Time</th><td><input type="time" name="eventTime"></td></tr>
+        <tr><th>Location</th><td><input type="text" name="location"></td></tr>
+        <tr><th>Agenda </th><td><input type="textarea" name="agenda"></td></tr>
         <tfoot><tr><td colspan="2"><input class = "sec-btn" type="reset" value="Clear">
           <input class = "pri-btn" type="submit" value="+ Add to Calendar"></td></tr></tfoot>
       </table>
@@ -94,7 +136,5 @@
     </div>
 <script type="text/javascript" src="js/calendar.js"></script>
 <script type="text/javascript" src="js/scheduler.js"></script>
-
-
 </body>
 </html>
