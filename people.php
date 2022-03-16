dc3257bc9cd470e0cb49d2bd6bdc330494b68191
cyfflying0125 <49615626+cyfflying0125@users.noreply.github.com>
2022-03-16 23:58:24 +0800

    Update people.php

--- people.php
+++ people.php
@@ -1,6 +1,7 @@
 <?php
   include 'connect.php';
   date_default_timezone_set('Asia/Singapore');
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,8 +9,8 @@
 		<meta charset=utf-8>
 		<link rel="shortcut icon" href="#" />
 		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <link rel="stylesheet" href="css/mastercss.css">
-    <link rel="stylesheet" href="css/peoplecss.css">
+    <link rel="stylesheet" type="text/css" href="css/mastercss.css">
+    <link rel="stylesheet" type="text/css" href="css/peoplecss.css">
 
   	<title>Wedding Planner</title>
 
@@ -38,21 +39,85 @@
 
     <div id="view">
 
-      <h2>Processional</h2>
-      <div class="card">
-      <img src="icon/heart.png" width="72" height="72">
-      <div class="container">
-        <h4><b>Groom</b></h4>
-        <p>Albus Weng</p>
-        <h4><b>Bride</b></h4>
-        <p>Charis Ng Xin Yi</p>
+      <?php
+        if(isset($_GET['viewmode']) && $_GET['viewmode'] == 'grid' ) {
+          ?>
+      <h2>Processional<div id="fullList" style="font-size: 18px;">
+        <a id="gridView" href="people.php?viewmode=grid">Grid View</a> |
+        <a id="ListView" href="people.php?viewmode=list">Full List</a></div>
+      </h2>
+      <br>
+      <div id="grid">
+      <?php
+      echo "<script>document.getElementById('gridView').style.fontWeight = 'bold';</script>";
+      $query = "SELECT * FROM people WHERE category = 'Processional' ORDER BY people.group ASC";
+      $result = $db->query($query);
+      $num_results = $result->num_rows;
+
+      $groupArray = [];
+      $roleArray = [];
+      $nameArray = [];
+      for($i=0; $i<$num_results; $i++) {
+        $row = $result->fetch_assoc();
+        $group = $row['group'];
+        $role = $row['role'];
+        $name = $row['name'];
+        array_push($groupArray, $group);
+        array_push($roleArray, $role);
+        array_push($nameArray, $name);
+      }
+
+
+      for ($j=1; $j<7; $j++) { //for each group, print a card
+        ?>
+        <div class="card">
+        <div class="container">
+        <span class="number"><?php echo $j;?></span>
+        <?php
+        foreach($groupArray as $key => $element) { //search array
+          if ($element == $j) { //if group number found
+            if ($roleArray[$key] != $titlePrinted) { //if role title not yet printed
+              echo "<h4><b>";
+              echo $roleArray[$key];
+              echo "</b></h4>";
+            }
+            echo "<p>";
+            echo $nameArray[$key];
+            echo "</p>";
+
+            $titlePrinted = $roleArray[$key];
+          }
+        }
+        ?>
+        </div>
+        </div>
+      <?php
+      }
+      ?>
       </div>
-</div>
       <h2>Services</h2>
+
+      <?php
+      $query = "SELECT * FROM people WHERE category = 'Service'";
+      $result = $db->query($query);
+      $num_results = $result->num_rows;
+      for($i=0; $i<$num_results; $i++) {
+        $row = $result->fetch_assoc();
+        ?>
+        <div class="card" style="height:140px; display: inline-block; ">
+        <div class="container">
+          <h4><b><?php echo $row['role'];?></b></h4>
+          <p><?php echo $row['name'];?></p>
+        </div>
+      </div>
+
+        <?php
+      }
+       ?>
       <h2>Guest Invitation</h2>
+    <?php
+    }?>
     </div>
-
-
     </div>
 
 </body>
