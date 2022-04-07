aa4c383e07b49b49cd72a63feabacb77df0619c0
cyfflying0125 <49615626+cyfflying0125@users.noreply.github.com>
2022-04-07 18:36:17 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,21 @@
 <?php
 include 'connect.php';
 
+if (isset($_POST['page'])) $page = $_POST['page'];
+//echo "<br>".$_POST['page'];
+
 if (isset($_POST['editArray'])) {
   $editArray = $_POST['editArray'];
-
-  //print_r($editArray);
-  //echo "<br>".$_POST['page'];
-
-} else echo "Error - Unable to Update the Database. Please Try Again Later.";
+  foreach($editArray as $key => $innerArray) {
+    //print_r($innerArray);
+    $itemID = $innerArray[0];
+    $col = $innerArray[1];
+    $value = str_replace('<br>','',$innerArray[2]); //Extract values from the sub-array
+    $subquery = "UPDATE $page SET $col = '$value' WHERE itemID = $itemID;";//Update query for the current element
+    $db->query($subquery);//Submit to Database
+    //echo $subquery."<br>";
+  }
+  $db -> close();
+}
 
 ?>
